e40d29bd51ab5f7c7b163303b95d514b023d1bb6
pcherng01 <LeafCherngchaosil@calvisitor-10-105-149-155.CalVisitor.1918.Berkeley.EDU>
2015-10-10 11:27:28 -0700

    Added the QR Scanning Code, need to integrate that to storyboard

---
+++ LetsEat/SCShapeView.h
@@ -0,0 +1,16 @@
+//
+//  SCShapeView.h
+//  LetsEat
+//
+//  Created by Pongsakorn Cherngchaosil on 10/10/15.
+//  Copyright © 2015 Monte Thakkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SCShapeView : UIView
+
+@property (nonatomic, strong) NSArray *corners;
+
+@end
